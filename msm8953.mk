6325d80b089754a52e76b08d43a278569989273f
Artem Borisov <dedsa2002@gmail.com>
2018-10-20 14:59:22 +0300

    Revert "DNM: changes to boot/build pie"
    This reverts commit 6d101f143c2595372dc97c41ff79e24fd4a349f8.

--- msm8953.mk
+++ msm8953.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-#PRODUCT_ENFORCE_RRO_TARGETS := \
+PRODUCT_ENFORCE_RRO_TARGETS := \
     Bluetooth \
     Settings \
     SettingsProvider \
